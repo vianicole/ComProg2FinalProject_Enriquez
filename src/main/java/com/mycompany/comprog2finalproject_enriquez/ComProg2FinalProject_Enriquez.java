bb6c5b37d653fa956a424c5284ef59806b90aa44
CL- <CL-@host.docker.internal>
2024-04-18 10:11:57 +0800

    proj

---
+++ src/main/java/com/mycompany/comprog2finalproject_enriquez/ComProg2FinalProject_Enriquez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.comprog2finalproject_enriquez;
+
+/**
+ *
+ * @author CL-
+ */
+public class ComProg2FinalProject_Enriquez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
